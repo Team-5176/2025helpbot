a6423ea0b41c47aac43a79c9cf92c565995f5e22
SLUH Robotics <robotics@sluh.org>
2025-02-24 21:32:53 -0600

    initial commit

---
+++ src/main/java/frc/robot/commands/SetArmPose.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import frc.robot.subsystems.testArm;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class SetArmPose extends Command {
+  private testArm arm;
+  private double setPoint;
+
+
+
+  /** Creates a new SetArmPose. */
+  public SetArmPose(testArm armSubsystem, double setPoint) {
+    this.arm = armSubsystem;
+    this.setPoint = setPoint;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    arm.setArmPose(setPoint);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return arm.isAtSetPoint();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/testArm.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+
+//import javax.lang.model.util.ElementScanner14;
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+//import com.revrobotics.spark.SparkRelativeEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class testArm extends SubsystemBase {
+  private SparkMax motorA = new SparkMax(12, MotorType.kBrushless);
+
+  private SparkClosedLoopController controllerA = motorA.getClosedLoopController();
+
+  private double setPoint = 0;
+
+  private RelativeEncoder encoderA = motorA.getEncoder();
+  
+
+  /** Creates a new testArm. */
+  public testArm() {
+    SparkMaxConfig config = new SparkMaxConfig();
+
+    config.idleMode(IdleMode.kBrake);
+    config.closedLoop.pid(0.01, 0, 0.002);
+
+    motorA.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+  }
+
+
+  public void setArmPose(double setPoint) {
+    this.setPoint = setPoint;
+    controllerA.setReference(setPoint, SparkBase.ControlType.kPosition);
+  }
+
+  public boolean isAtSetPoint() {
+    if((setPoint - 1 < encoderA.getPosition()) && (encoderA.getPosition() < setPoint + 1))
+      return true;
+    else 
+      return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("ArmPoseA", encoderA.getPosition());
+    SmartDashboard.putNumber("Arm setPoint", setPoint);
+    SmartDashboard.putBoolean("ArmAtSetPoint", isAtSetPoint());
+  }
+}
