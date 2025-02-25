a78b445a674bf871639ea6186958a00af2d9a197
Fernando Alvear <falvear210@gmail.com>
2025-02-24 22:04:11 -0600

    some cleaning

--- src/main/java/frc/robot/subsystems/testArm.java
+++ src/main/java/frc/robot/subsystems/testArm.java
@@ -7,13 +7,10 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 
-//import javax.lang.model.util.ElementScanner14;
-
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkClosedLoopController;
-//import com.revrobotics.spark.SparkRelativeEncoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
